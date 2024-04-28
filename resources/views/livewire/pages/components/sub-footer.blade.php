e00a453c4fcfaaca6c22349b6a92d293e1dae7f5
VictorDev <victordasilvafernandes@gmail.com>
2024-04-28 15:28:58 -0300

    feat: finishing frontend

--- resources/views/livewire/pages/components/sub-footer.blade.php
+++ resources/views/livewire/pages/components/sub-footer.blade.php
@@ -1,8 +1,8 @@
 
 <div class="container flex justify-between items-center mx-auto bg-base-blue rounded-xl py-3 px-5 sm:px-10 text-white translate-y-1/2 gap-x-6">
     <div class="">
-        <h2 class="txt-subtitle text-2xl">Need more information?</h2>
-        <p class="txt-base text-md mt-2">Write your concern to us and our specialist will get back to you.</p>
+        <h2 class="txt-subtitle text-2xl">Quer mais informações?</h2>
+        <p class="txt-base text-md mt-2">Escreva sua dúvida para nós e nosso especialista entrará em contato com você.</p>
     </div>
     <div class="">
         <a href="{{ route('web.home') }}#contact" class="px-4 py-2 sm:px-8 sm:py-4 duration-300 text-sm md:text-lg font-semibold bg-base-red hover:bg-opacity-70 text-white whitespace-nowrap">Ver mais</a>
